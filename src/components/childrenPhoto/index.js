05ab309ccdaf17c83614dcd3540448b6b993ff9a
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2022-12-03 15:28:56 +0300

    create photoSlider component

--- src/components/childrenPhoto/index.js
+++ src/components/childrenPhoto/index.js
@@ -1,30 +1,11 @@
-import React, { useRef } from "react";
+import React from "react";
 import styles from "./photo.module.scss";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/grid";
-import "./styles.css";
-// import required modules
-import { Grid, Navigation,Pagination } from "swiper";
-
-import Fancybox from "../../utils/fancybox";
-
-const apiUrl = "http://theatre.restomatik.ru:1337";
-const screen_width = window.screen.width;
+import PhotoSlider from "../photoSlider"
 
 export default function ChildrenPhoto(props) {
   const { id, items } = props;
-  const navigationPrevRef = useRef(null);
-  const navigationNextRef = useRef(null);
 
-  // slider parameters for mobile
-  const rows = screen_width > 500 ? 2 : 1;
-  const slidesPerView = screen_width > 500 ? 3 : "auto";
-  const navigation =  screen_width > 500 ? {prevEl: navigationPrevRef.current, nextEl: navigationNextRef.current,} : false;
-  const pagination =  screen_width > 500 ? false : {clickable: true, dynamicBullets: true};
-  const centeredSlides = screen_width > 500 ? false : true;
   return (
     <>
       <section id={id}>
@@ -34,60 +15,7 @@ export default function ChildrenPhoto(props) {
               <h2>Фотографии со спектаклей</h2>
             </div>
             <div className={styles.photoBlock}>
-              <Swiper
-                slidesPerView={slidesPerView}
-                centeredSlides={centeredSlides}
-                grid={{
-                  rows: rows,
-                }}
-                spaceBetween={20}
-                navigation={navigation}
-                pagination={pagination}
-                modules={[Grid, Navigation, Pagination]}
-                className="photoSwiper"
-                style={{
-                  "--swiper-pagination-bullet-inactive-color": "#fff",
-                  "--swiper-pagination-color": "#8CABFA",
-                  "--swiper-pagination-bullet-inactive-opacity": "0.6"
-                }}
-              >
-                <Fancybox>
-                  {items.map((item, key) => (
-                    <SwiperSlide key={key}>
-                      <a
-                        data-fancybox="gallery"
-                        href={apiUrl + item.media.data.attributes.url}
-                        data-caption={item.caption}
-                        className={styles.sliderLink}
-                      >
-                        <img
-                          className={styles.sliderImg}
-                          alt=""
-                          src={
-                            apiUrl +
-                            item.media.data.attributes.formats.small.url
-                          }
-                        />
-                      </a>
-                    </SwiperSlide>
-                  ))}
-                </Fancybox>
-              </Swiper>
-              <div className={styles.sliderNavigation}>
-                <img
-                  src="/img/newsLarr.png"
-                  alt="<"
-                  className={styles.prev}
-                  ref={navigationPrevRef}
-                />
-
-                <img
-                  src="/img/newsRarr.png"
-                  alt=">"
-                  className={styles.next}
-                  ref={navigationNextRef}
-                />
-              </div>
+              <PhotoSlider items={items} />
             </div>
           </div>
         </div>
