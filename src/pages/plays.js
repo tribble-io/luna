ebc5c464ac0fb29c08bc4be505661793399db37f
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2022-12-06 17:40:04 +0300

    created api folder for api requests

--- src/pages/plays.js
+++ src/pages/plays.js
@@ -1,34 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { api } from "../api/index";
 
 import ShowsFilter from "../components/plays/filters";
 import ShowsCards from "../components/plays/cards";
 import Loader from "../components/loader"
-
-const playsApi = "http://theatre.restomatik.ru:1337/api/plays";
-
-function getUrl(editValue) {
-  let filters = "";
-
-  if (editValue.title.length > 0) {
-    filters += `filters[title][$containsi]=${editValue.title}&`;
-  }
-
-  if (editValue.scene.length > 0) {
-    filters += `filters[scene][$eq]=${editValue.scene}&`;
-  }
-
-  if (editValue.rating === true) {
-    filters += `filters[rating][$lt]=16&`;
-  }
-
-  if (editValue.isPremiere === true) {
-    filters += `filters[isPremiere][$eq]=true&`;
-  }
-
-  filters += filters[filters.length - 1] !== "&" ? "&" : "";
-  return `${playsApi}?${filters}populate=cover`;
-}
 
 function Plays() {
   const [items, setItems] = useState([]);
@@ -39,21 +14,17 @@ function Plays() {
     rating: false,
     isPremiere: false,
   });
-  const url = getUrl(editValue);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const itemsResponse = await axios.get(url);
+    api.exportPlayShows(editValue)
+      .then((response) => {
+        setItems(response);
         setIsLoading(false);
-        setItems(itemsResponse.data.data);
-      } catch (error) {
-        alert("Ошибка при запросе данных!");
-        console.error(error);
-      }
-    }
-
-    fetchData();
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
   }, [editValue]);
 
   return (
