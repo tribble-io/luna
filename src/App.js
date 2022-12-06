52b19630c4bdbb56a15a66afc67fe912ab778021
KuKupwa <cat201223@ya.ru>
2022-12-06 23:47:51 +0300

    Add theathre history page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import "./App.css";
-import Header from "./components/header";
-import Nav from "./components/nav";
-import Block from "./components/blockFooter";
-import LastBlock from "./components/blockFooterLast";
-import Plays from "./pages/plays";
-import Home from "./pages";
-import ChildrenStudio from "./pages/children-studio";
+import './App.css';
+import Header from './components/header';
+import Nav from './components/nav';
+import Block from './components/blockFooter';
+import LastBlock from './components/blockFooterLast';
+import Plays from './pages/plays';
+import Home from './pages';
+import ChildrenStudio from './pages/children-studio';
+import HistoryTheathre from './components/historyTheathre';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Header />
-        <Nav />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/plays" element={<Plays />} />
-          <Route path="/children-studio" element={<ChildrenStudio />} />
-        </Routes>
-        <footer>
-          <Block />
-          <LastBlock />
-        </footer>
-      </Router>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Header />
+                <Nav />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/plays" element={<Plays />} />
+                    <Route path="/children-studio" element={<ChildrenStudio />} />
+                    <Route path="/theatre-history" element={<HistoryTheathre />} />
+                </Routes>
+                <footer>
+                    <Block />
+                    <LastBlock />
+                </footer>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
