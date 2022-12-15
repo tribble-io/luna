a2a7ec148f29d7aec8b23a84be0218d90b713d0b
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2022-12-15 22:46:57 +0300

    delete black version

--- src/components/contact/map/index.js
+++ src/components/contact/map/index.js
@@ -17,18 +17,7 @@ export function ContactMap() {
           </p>
         </div>
       </div>
-      <div className={styles.mapContentV1}>
-        <iframe
-          src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d561.6082695610156!2d37.6260493!3d55.7336039!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b1cb353adcb%3A0x8c3c08d5caea685e!2z0KLQtdCw0YLRgCDQm9GD0L3Riw!5e0!3m2!1sru!2s!4v1671129907656!5m2!1sru!2s'
-          width='100%'
-          height='100%'
-          style={{ border: '0', width: '100%', height: '100%' }}
-          allowFullScreen=''
-          loading='lazy'
-          referrerPolicy='no-referrer-when-downgrade'
-        ></iframe>
-      </div>
-      <div className={styles.mapContentV2}>
+      <div className={styles.mapContent}>
         <iframe
           src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d561.6082695610156!2d37.6260493!3d55.7336039!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b1cb353adcb%3A0x8c3c08d5caea685e!2z0KLQtdCw0YLRgCDQm9GD0L3Riw!5e0!3m2!1sru!2s!4v1671129907656!5m2!1sru!2s'
           width='100%'
