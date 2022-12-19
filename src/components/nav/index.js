5c2bcc64cf385c4034770f36ff17c6aa0ed7dd99
Alex-Wolf-front <vol4ara.ukr@gmail.com>
2022-12-20 00:16:17 +0300

    new header

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useWindowScrollPositions } from '../../assets/utils/usable-function'
 import styles from './nav.module.scss'
@@ -6,30 +6,50 @@ import styles from './nav.module.scss'
 function Nav() {
   const { scrollY } = useWindowScrollPositions()
   const opacityEl = window.screen.width > 1200 ? (scrollY >= 200 ? 1 : 0) : 1
+  const [isShown, setIsShown] = useState(false)
 
   return (
     <nav>
       <div className={styles.wrapper}>
         <ul>
-          <li>
+          <li className={styles.menuItem}>
             <Link to='/playbill'>Афиша</Link>
           </li>
-          <li>
+          <li className={styles.menuItem}>
             <Link to='/plays'>Спектакли</Link>
           </li>
-          <li>
-            <Link to='/troupe'>Труппа</Link>
+          <li
+            className={styles.menuItem}
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+          >
+            О театре
+            <div
+              className={`${isShown ? styles.active : ''} ${
+                styles.dropdownMenu
+              }`}
+            >
+              <div className={styles.wrapper}>
+                <div className={styles.subMenuBlock}>
+                  <Link to='/history'>История театра</Link>
+                  <Link to='/troupe'>Творческий состав</Link>
+                  <Link to='/children-studio'>
+                    Детская театральная студия &#171;Маленькая Луна&#187;
+                  </Link>
+                </div>
+                <div className={styles.subMenuBlock}>
+                  <Link to='/romaska-awards'>Премия &#171;Ромашка&#187;</Link>
+                  <Link to='/'>Сцены</Link>
+                  <Link to='/'>Вакансии</Link>
+                  <Link to='/press'>Пресса о театре</Link>
+                </div>
+              </div>
+            </div>
           </li>
-          <li>
-            <Link to='/history'>театр</Link>
-          </li>
-          <li>
+          <li className={styles.menuItem}>
             <Link to='/news'>Новости</Link>
           </li>
-          <li>
-            <Link to='/press'>Пресса</Link>
-          </li>
-          <li>
+          <li className={styles.menuItem}>
             <Link to='/contacts'>Контакты</Link>
           </li>
         </ul>
