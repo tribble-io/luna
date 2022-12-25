f1160759c42d65cf4a6a5fb974cf3f9e6f118942
Evgeniy Panchenko <evgeniypanchenko@Evgeniys-Air.Dlink>
2022-12-26 01:10:37 +0300

    fixes logo, mobile menu and minor fixes

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -5,17 +5,24 @@ import imgLunaMobile from './img/back-not-found-mobile.jpg'
 import styles from './PageNotFound.module.scss'
 
 const PageNotFound = () => {
+  const handelClickBack = () => {
+    history.back()
+  }
   return (
     <section className={styles.containerNotFound}>
       <div className={styles.notFound}>
         <h2>{`упс... ${'  '} ошибка`}</h2>
         <p>
-          К сожалению, запрашиваемая страница не найдена. Возможно, Вы перешли
-          по ссылке, в которой была допущена ошибка, или ресурс был удален.
-          Попробуйте перейти на{' '}
+          К сожалению, страница, которую Вы пытаетесь найти, удалена или
+          недоступна. Мы скоро разберемся с этой проблемой, а пока Вы можете
+          вернуться на{' '}
           <Link className={styles.link} to={'/'}>
-            главную страницу
-          </Link>
+            главную
+          </Link>{' '}
+          или на{' '}
+          <span className={styles.linkBack} onClick={() => handelClickBack()}>
+            предыдущую страницу.
+          </span>{' '}
         </p>
         <img src={imgLunaMobile} className={styles.imageLuna} alt={'Луна'} />
       </div>
